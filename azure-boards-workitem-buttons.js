07cf92a17a383d3e17ecb2b8e3f20f127862565b
Ben Oeyen <ben.oeyen@gmail.com>
2026-05-08 12:19:40 +0200

    Add Colorblind Mode.

--- azure-boards-workitem-buttons.js
+++ azure-boards-workitem-buttons.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Azure Boards - Workitem Buttons
 // @namespace    https://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @author       Ben Oeyen
-// @description  Azure Boards - Generate GIT branch, Commit message and Ticket Announcement and copy to clipboard, Order Discussions,..
+// @description  Azure Boards - Generate GIT branch, Commit message and Ticket Announcement and copy to clipboard, Order Discussions, Colorblind Mode
 // @match        https://dev.azure.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://az-icons.com/
 // @downloadURL  https://github.com/Jarflux/tampermonkey-hubo/raw/master/azure-boards-workitem-buttons.js
@@ -24,21 +24,26 @@
         cards.forEach(card => {
             // Find the title text span
             const titleSpan = card.querySelector('.title-text');
-
-            if (!titleSpan) return;
-
-            // Get the color from the title span style
-            const titleColor = titleSpan.style.color;
-
-            // Apply the color to the card flag
             const flag = card.querySelector('.card-flag');
-            if (flag && titleColor) {
-                flag.style.backgroundColor = titleColor;
+            if(titleSpan){
+                // Get the color from the title span style
+                const titleColor = titleSpan.style.color;
+
+                // Apply the color to the card flag
+                if (flag && titleColor) {
+                    flag.style.backgroundColor = titleColor;
+                }
+
+                // Remove bold and color from title text
+                titleSpan.style.fontWeight = 'normal';
+                titleSpan.style.color = '';
             }
 
-            // Remove bold and color from title text
-            titleSpan.style.fontWeight = 'normal';
-            titleSpan.style.color = '';
+            if(isColorBlindMode()){
+                applyColorBlindLabels(flag);
+            }else{
+                removeColorBlindLabels(flag);
+            }
         });
     }
 
@@ -55,6 +60,98 @@
             container.prepend(commitButton(false));
             container.prepend(gitBranchButton(false));
         }
+    }
+
+    // Colorblind Mode
+    // ==================
+    const COLORBLIND_STORAGE_KEY = 'tampermonkey-colorblind-mode';
+
+    function isColorBlindMode() {
+        if(!localStorage.getItem(COLORBLIND_STORAGE_KEY)){
+            setPreference(false);
+        }
+        return localStorage.getItem(COLORBLIND_STORAGE_KEY) == "true";
+    }
+
+    function setColorBlindMode(value) {
+        localStorage.setItem(COLORBLIND_STORAGE_KEY, value);
+    }
+
+    function applyColorBlindLabels(flag){
+        flag.style.color = "white";
+        flag.style.width = "18px";
+        flag.style.writingMode = "vertical-rl";
+        flag.style.fontWeight = "bold";
+        flag.style.padding = "5px 1px 5px 1px";
+        const flagColor = flag.style.backgroundColor;
+
+        if( flagColor == "rgb(240, 102, 115)" ){
+            flag.textContent = "MUST";
+        }
+        if( flagColor == "rgb(168, 206, 75)") {
+            flag.textContent = "COULD";
+        }
+        if( flagColor == "rgb(249, 185, 120)") {
+            flag.textContent = "SHOULD";
+        }
+        if( flagColor == "rgb(0, 152, 199)"){
+            flag.textContent = "NO MOSCOW";
+        }
+        if( flagColor == "rgb(204, 41, 61)"){
+            flag.textContent = "BUG";
+        }
+    }
+
+    function removeColorBlindLabels(flag){
+        flag.textContent = "";
+        flag.style.width = "4px";
+    }
+
+    function addColorblindModeButton() {
+        const btn = document.createElement('button');
+
+        btn.id = 'tampermonkey-colorblind-btn';
+        btn.className ='bolt-header-command-item-button bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment';
+        btn.setAttribute('role', 'button');
+        btn.setAttribute('tabindex', '0');
+        btn.type = 'button';
+
+        const spanIconWrapper = document.createElement('span');
+        spanIconWrapper.className = 'fluent-icons-enabled';
+
+        const icon = document.createElement('span');
+        icon.className = 'left-icon flex-noshrink fabric-icon ms-Icon--Color medium';
+
+        spanIconWrapper.appendChild(icon);
+        btn.appendChild(spanIconWrapper);
+
+        btn.onclick = (event) => {
+            event.stopPropagation();
+            setColorBlindMode(!isColorBlindMode());
+            updateCards();
+        };
+        return btn;
+    }
+
+
+    function addColorBlindButtonToBoardControls() {
+        if (document.getElementById('tampermonkey-colorblind-btn')) return true;
+
+        console.log("Add ColorBlind Button");
+        const controls = document.querySelector('.boards-tabs-header .bolt-header-commandbar .bolt-header-commandbar-button-group');
+        if (!controls) return false;
+        console.log("Add ColorBlind Button- cont");
+
+        const btn = addColorblindModeButton();
+        const lastButton = controls.querySelector('.overflow-button-empty-div');
+
+        controls.insertBefore(btn, lastButton);
+    }
+    // ==================
+
+
+    function getUserEmail(){
+        return document.getElementById("mectrl_currentAccount_secondary")? document.getElementById("mectrl_currentAccount_secondary").textContent: null;
     }
 
     function gitBranchButton(disabled) {
@@ -242,19 +339,37 @@
 
     function start() {
         const observer = new MutationObserver(() => {
+            console.log("mutation triggered");
+            stopObserver();
+            addColorBlindButtonToBoardControls();
             addButtonsToWorkItem();
             updateCards();
             initDiscussionOrder();
+            startObserver();
         });
 
         navigation.addEventListener("navigate", (event) => {
             firstLoad = true;
+            stopObserver();
+            addColorBlindButtonToBoardControls();
             addButtonsToWorkItem();
             updateCards();
             initDiscussionOrder();
+            startObserver();
         });
 
-        observer.observe(document.body, {childList: true, subtree: true});
+        function stopObserver(){
+            observer.disconnect();
+        }
+
+        function startObserver() {
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+            });
+        }
+
+        observeAgain();
     }
 
     start();
